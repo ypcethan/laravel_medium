8a49a627392e87b3ba8efc1245109bb28aa27403
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-06 13:44:16 +0800

    correct the domain name for axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,17 @@ import storeData from "./store/storeData";
 import Vuex from "vuex";
 window.Vue = require("vue");
 
-console.log("ENV");
-console.log(process.env.MIX_APP_ENV);
-console.log("URL");
-console.log(process.env.MIX_APP_URL);
+// console.log("ENV");
+// console.log(process.env.MIX_APP_ENV);
+// console.log("URL");
+// console.log(process.env.MIX_APP_URL);
 const files = require.context("./", true, /\.vue$/i);
 files.keys().map(key => {
     Vue.component(
         key
-            .split("/")
-            .pop()
-            .split(".")[0],
+        .split("/")
+        .pop()
+        .split(".")[0],
         files(key).default
     );
 });
